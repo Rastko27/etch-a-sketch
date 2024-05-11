08f918476476a758ee195146f9a167feff603f14
Rastko <rastko.strbac@gmail.com>
2024-05-11 14:37:20 +0200

    Add button and dynamic grid size
    Add button which prompts to enter grid size. Create grid based on
    entered grid size.

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
+const button = document.querySelector(".btn");
+let n;
+button.addEventListener("click", () => {
+    n = prompt("Enter grid size:", 16);
+    if(n !== null && !isNaN(n) && n > 0 && n<=100) { // Check if n is valid
+        makeGrid();
+    }
+    else{
+        alert("Please enter a valid grid size.");
+    }
+});
+
 const container = document.querySelector(".container");
-let i,j;
-for(i=0;i<15;i++){
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for(j=0;j<15;j++){
-        const gridSingle = document.createElement("div");
-        gridSingle.classList.add("grid-single");
-        row.appendChild(gridSingle);
+function makeGrid(){
+    container.innerHTML = ""; // Clears grid
+    let gridSize = 700/n;
+    let i,j;
+    for(i=0;i<n;i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for(j=0;j<n;j++){
+            const gridSingle = document.createElement("div");
+            gridSingle.classList.add("grid-single");
+            gridSingle.style.width = gridSize + "px";
+            gridSingle.style.height = gridSize + "px";
+            row.appendChild(gridSingle);
+            gridSingle.addEventListener("mouseover", () => {
+                gridSingle.style.opacity = "0.5";
+            });
+        }
     }
 }
-const gridSingles = document.querySelectorAll(".grid-single");
-gridSingles.forEach(gridSingle => {
-    gridSingle.addEventListener("mouseover", () => {
-        gridSingle.style.opacity = "0.5";
-    });
-})
